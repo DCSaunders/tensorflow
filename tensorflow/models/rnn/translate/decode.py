18a5cf6f68631a2b0c1f1e0243dcec10dd1a9da6
DCSaunders <ds636@cam.ac.uk>
2016-12-17 14:51:30 +0000

    decode interpolated sets, logging appropriately

--- tensorflow/models/rnn/translate/decode.py
+++ tensorflow/models/rnn/translate/decode.py
@@ -27,6 +27,7 @@ tf.app.flags.DEFINE_string("test_hidden_state", "/tmp/hidden.txt", "Output file 
 tf.app.flags.DEFINE_integer("max_sentences", 0, "The maximum number of sentences to translate (all if set to 0)")
 tf.app.flags.DEFINE_string("decode_hidden", None, "Decode from hidden layers in file")
 tf.app.flags.DEFINE_boolean("interactive", False, "Decode from command line")
+tf.app.flags.DEFINE_string("decode_interpolate_hidden", None, "Decode from hidden layers interpolating between true and generated hidden vectors")
 tf.app.flags.DEFINE_string("model_path", None, "Path to trained model")
 FLAGS = tf.app.flags.FLAGS
 
@@ -42,8 +43,15 @@ def decode(config, input=None, output=None, max_sentences=0):
   else:
     hidden = '/tmp/hidden'
   
+  max_sents = 0
+  if 'max_sentences' in config:
+    max_sents = config and config['max_sentences']
+  if max_sentences > 0:
+    max_sents = max_sentences
   if config['decode_hidden']:
-    decode_hidden(config, out)
+    unpickle_hidden(config, out, max_sentences=max_sents)
+  elif config['decode_interpolate_hidden']:
+    decode_interpolate_hidden(config, out, max_sentences=max_sents)
   else:
     # Find longest input to create suitable bucket
     max_input_length = 0
@@ -67,12 +75,6 @@ def decode(config, input=None, output=None, max_sentences=0):
 
       # Decode input file
       num_sentences = 0
-      max_sents = 0
-      if 'max_sentences' in config:
-        max_sents = config and config['max_sentences']
-      if max_sentences > 0:
-        max_sents = max_sentences
-
       logging.info("Start decoding, max_sentences=%i" % max_sents)
       with open(inp) as f_in, open(out, 'w') as f_out, open(hidden, 'wb') as f_hidden:
         pickler = cPickle.Pickler(f_hidden)
@@ -83,36 +85,70 @@ def decode(config, input=None, output=None, max_sentences=0):
           if data_utils.EOS_ID in outputs:
             outputs = outputs[:outputs.index(data_utils.EOS_ID)]
           print(" ".join([str(tok) for tok in outputs]), file=f_out)
-          pickler.dump(states)
+          pickler.dump({'states': states, 'length': len(outputs)})
           num_sentences += 1
           if max_sents > 0 and num_sentences >= max_sents:
             break
   logging.info("Decoding completed.")
 
-def decode_hidden(config, out):
+def unpickle_hidden(config, out, max_sentences=0):
+  hidden_list = []
+  with open(config['decode_hidden'], 'rb') as f_in:
+    unpickler = cPickle.Unpickler(f_in)
+    hidden_size = config['hidden_size']
+    while True and (max_sentences == 0 or len(hidden_list) < max_sentences):
+      try:
+        hidden = np.array(unpickler.load()['states'])
+        hidden = hidden.reshape(1, 2*hidden_size) # batch_size, BiRNN size
+        hidden_list.append(hidden)
+      except (EOFError):
+        break
   with tf.Session() as session:
-    model = model_utils.create_model(session, config,
-                                     forward_only=True, hidden=True)
+    model = model_utils.create_model(session, config, forward_only=True, hidden=True)
     model.batch_size = 1  # We decode one sentence at a time.
-    with open(config['decode_hidden'], 'rb') as f_in, open(config['test_out_idx'], 'w') as f_out:
-      unpickler = cPickle.Unpickler(f_in)
-      num_layers = config['num_layers']
-      hidden_size = config['hidden_size']
-      while True:
-        try:
-          #hidden = np.random.randn(1, 1000)
-          hidden = np.array(unpickler.load())
-          hidden = hidden.reshape(model.batch_size, 2*hidden_size)
-          #logging.info("Hidden: {}".format(hidden))
-          #inp = ' '.join(list(str(num) for num in np.random.randint(3, high=100, size=7))) # random dummy input
-          outputs, states = get_outputs(session, config, model,
-                                    sentence='', hidden=hidden)
-          logging.info("Output: {}".format(outputs))
-          if data_utils.EOS_ID in outputs:
-            outputs = outputs[:outputs.index(data_utils.EOS_ID)]
-          print(" ".join([str(tok) for tok in outputs]), file=f_out)
-        except (EOFError):
-          break
+
+    decode_hidden(session, model, config, out, hidden_list)
+
+def decode_interpolate_hidden(config, out, max_sentences=0):
+  hidden_list = []
+  hidden_size = config['hidden_size']
+  num_decoded = 0
+  with tf.Session() as session:
+    model = model_utils.create_model(session, config, forward_only=True, hidden=True)
+    model.batch_size = 1  # We decode one sentence at a time.
+
+    with open(config['decode_interpolate_hidden'], 'rb') as f_in:
+      label_samples = cPickle.load(f_in)
+      for label in label_samples:
+        log_msg(config['test_out_idx'],
+                'Gaussian mixture component: {}\n'.format(label))
+        for interp_list in label_samples[label]:
+          log_msg(config['test_out_idx'], 'New interpolation set\n')
+          for i in range(0, len(interp_list)):
+            interp_list[i] = interp_list[i].reshape(1, 2*hidden_size)
+          decode_hidden(session, model, config, out, interp_list, append=True)
+          num_decoded += 1
+          if num_decoded > max_sentences and max_sentences > 0:
+            break
+     
+def log_msg(f_name, msg):
+  with open(f_name, 'a') as f_out:
+    f_out.write(msg)
+
+def decode_hidden(session, model, config, out, hidden_list, append=False):
+  if append:
+    mode = 'a'
+  else:
+    mode = 'w'
+  with open(config['test_out_idx'], mode) as f_out:
+    for hidden in hidden_list:
+      #hidden = np.random.randn(1, 1000)
+      outputs, states = get_outputs(session, config, model, sentence='', hidden=hidden)
+      logging.info("Output: {}".format(outputs))
+      if data_utils.EOS_ID in outputs:
+        outputs = outputs[:outputs.index(data_utils.EOS_ID)]
+      print(" ".join([str(tok) for tok in outputs]), file=f_out)
+
 
 def decode_interactive(config):
   with tf.Session() as session:
@@ -141,8 +177,11 @@ def get_outputs(session, config, model, sentence, buckets=None, hidden=None):
 
   if not buckets:
     buckets = model_utils._buckets
-  bucket_id = min([b for b in xrange(len(buckets))
-                  if buckets[b][0] >= len(token_ids)])
+  if hidden is None:
+    bucket_id = min([b for b in xrange(len(buckets))
+                     if buckets[b][0] >= len(token_ids)])
+  else:
+    bucket_id = max([b for b in xrange(len(buckets))])
   logging.info("Bucket {}".format(buckets[bucket_id]))
   logging.info("Input: {}".format(token_ids))
 
