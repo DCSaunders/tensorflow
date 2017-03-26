fixed kl loss, scheduled sampling

tensorflow/models/rnn/translate/seq2seq/seq2seq_model.py
@@ -124,6 +124,7 @@ class Seq2SeqModel(object):
     self.scheduled_sample_steps = 1000
     self.kl_min = kl_min
     self.word_keep_prob = word_keep_prob
+    self.anneal_scale = tf.placeholder(tf.float32, shape=[])
 
     # If we use sampled softmax, we need an output projection.
     output_projection = None
@@ -201,7 +202,8 @@ class Seq2SeqModel(object):
         scope = variable_prefix+"/embedding_attention_seq2seq"
       logging.info("Using variable scope {}".format(scope)) 
 
-    def seq2seq_f(encoder_inputs, decoder_inputs, do_decode, bucket_length, encoder_state=None):
+    def seq2seq_f(encoder_inputs, decoder_inputs, do_decode, bucket_length, encoder_state=None,
+                  feed_prev_p=None):
       seq2seq_args = dict(encoder_inputs=encoder_inputs,
                           decoder_inputs=decoder_inputs,
                           cell=cell, 
@@ -218,15 +220,18 @@ class Seq2SeqModel(object):
       if self.seq2seq_mode == 'autoencoder':
         logging.info("Creating embedding rnn autoencoder")
         seq2seq_args.update(num_symbols=source_vocab_size,
-                            hidden_state=encoder_state)
+                            hidden_state=encoder_state,
+                            feed_prev_p=feed_prev_p)
         return tf.nn.seq2seq.embedding_rnn_autoencoder_seq2seq(**seq2seq_args)
-                    
       elif self.seq2seq_mode == 'vae':
         logging.info("Creating embedding rnn variational autoencoder")
         seq2seq_args.update(num_symbols=source_vocab_size,
                             latent_size=latent_size,
                             transfer_func=tf.nn.relu,
-                            latent_state=encoder_state)
+                            latent_state=encoder_state,
+                            anneal_scale=self.anneal_scale,
+                            kl_min=self.kl_min,
+                            feed_prev_p=feed_prev_p)
         return tf.nn.seq2seq.embedding_rnn_vae_seq2seq(**seq2seq_args)
       else:
         logging.info('Creating embedding attention model')
@@ -244,7 +249,7 @@ class Seq2SeqModel(object):
     self.encoder_inputs = []
     self.encoder_states = tf.placeholder(tf.float32, shape=[1, None])
     self.decoder_inputs = []
-    self.decoder_src = []
+    self.feed_prev_p = tf.placeholder(tf.float32, shape=[])
     self.target_weights = []
     self.targets = []
       
@@ -254,8 +259,6 @@ class Seq2SeqModel(object):
     for i in xrange(buckets[-1][1] + 1):
       self.decoder_inputs.append(tf.placeholder(tf.int32, shape=[None],
                                                 name="decoder{0}".format(i)))
-      self.decoder_src.append(tf.placeholder(tf.int32, shape=[None],
-                                             name="decoder_src{0}".format(i)))
       self.target_weights.append(tf.placeholder(dtype, shape=[None],
                                                 name="weight{0}".format(i)))
       self.targets.append(tf.placeholder(tf.int32, shape=[None],
@@ -283,31 +286,23 @@ class Seq2SeqModel(object):
 
     # Training outputs and losses.
     def adjust_loss_vae():
-      self.anneal_scale = tf.placeholder(tf.float32, shape=[], name="anneal_scale")
       self.reconstruct_loss = [tf.placeholder(tf.float32, shape=[]) for _ in range(len(self.losses))]
       self.kl_loss = [tf.placeholder(tf.float32, shape=[]) for _ in range(len(self.losses))]
       for b in range(len(self.losses)):
         self.reconstruct_loss[b] = self.losses[b][0]
-        self.kl_loss[b] = self.losses[b][1]
-        if self.kl_min > 0.0:
-          self.kl_loss[b] = max(self.kl_min, self.kl_loss[b])
-        self.losses[b] = tf.add(self.reconstruct_loss[b], tf.mul(self.anneal_scale, self.kl_loss[b]))
+        self.kl_loss[b] = self.losses[b][1] 
+        self.losses[b] = tf.add(self.reconstruct_loss[b], self.kl_loss[b])
 
-    model_args = {'encoder_inputs': self.encoder_inputs, 'decoder_inputs': self.decoder_inputs, 
-            'targets': self.targets, 'weights': self.target_weights, 'buckets': buckets, 
-            'seq2seq': lambda x, y, z: seq2seq_f(x, y, False, z),
-            'softmax_loss_function': softmax_loss_function}
-    if self.seq2seq_mode == 'vae':
-      bucket_model = tf.nn.seq2seq.vae_with_buckets_states
-    else:
-      bucket_model = tf.nn.seq2seq.model_with_buckets_states
+    state = self.encoder_states if hidden else None
+    feed_prev = self.feed_prev_p if (self.scheduled_sample and not forward_only) else None
+    model_args = dict(encoder_inputs=self.encoder_inputs, decoder_inputs=self.decoder_inputs, 
+                      targets=self.targets, weights=self.target_weights, buckets=buckets, 
+                      seq2seq=lambda a, b, c, d, e: seq2seq_f(a, b, False, c, d, e),
+                      softmax_loss_function=softmax_loss_function,
+                      encoder_states=state, feed_prev_p=feed_prev)
     if forward_only:
-      if hidden:
-        model_args['seq2seq'] = lambda a, b, c, d: seq2seq_f(a, b, True, c, d)
-        model_args['encoder_states'] = self.encoder_states
-      else:
-        model_args['seq2seq'] = lambda x, y, z: seq2seq_f(x, y, True, z)
-      self.outputs, self.losses, self.states = bucket_model(**model_args)
+      model_args.update(seq2seq=lambda a, b, c, d, e: seq2seq_f(a, b, True, c, d, e))
+      self.outputs, self.losses, self.states = tf.nn.seq2seq.model_with_buckets_states(**model_args)
       # If we use output projection, we need to project outputs for decoding.
       if output_projection is not None:
         for b in xrange(len(buckets)):
@@ -317,7 +312,7 @@ class Seq2SeqModel(object):
           self.outputs[b] = [tf.matmul(output, output_projection[0]) + output_projection[1]
                              for output in self.outputs[b]]
     else:
-      self.outputs, self.losses, self.states = bucket_model(**model_args)
+      self.outputs, self.losses, self.states = tf.nn.seq2seq.model_with_buckets_states(**model_args)
     if self.seq2seq_mode == 'vae':
       adjust_loss_vae()
 
@@ -393,26 +388,15 @@ class Seq2SeqModel(object):
         return out
       else:
         return words
-      
-    def schedule(words):
-      # Boolean markers for linear scheduled sampling
-      feed_prev = []
-      p_feed_prev = min(1.0, self.global_step.eval()/self.scheduled_sample_steps)
-      for word in words:
-        if word == data_utils.GO_ID or np.random.uniform() < p_feed_prev:
-          feed_prev.append(False)
-        else:
-          feed_prev.append(True)
-      return feed_prev
-      
+        
     # Input feed: encoder inputs, decoder inputs, target_weights, as provided.
     input_feed = {}
     for l in xrange(encoder_size):
       input_feed[self.encoder_inputs[l].name] = encoder_inputs[l]
     for l in xrange(decoder_size):
       input_feed[self.decoder_inputs[l].name] = word_dropout(decoder_inputs[l])
-      if self.scheduled_sample:
-        input_feed[self.decoder_src[l].name] = schedule(decoder_inputs[l])
+      if self.scheduled_sample and not forward_only:
+        input_feed[self.feed_prev_p.name] = min(1.0, self.global_step.eval() / self.scheduled_sample_steps)
       input_feed[self.target_weights[l].name] = target_weights[l]
       if l < decoder_size - 1:
         input_feed[self.targets[l].name] = decoder_inputs[l + 1]
@@ -429,11 +413,9 @@ class Seq2SeqModel(object):
     if bow_mask is not None:
       logging.debug("Using bow mask for decoder: feed")
       input_feed[self.bow_mask.name] = bow_mask
-
     if hidden is not None:
       logging.debug("Decoding from hidden layer")
       input_feed[self.encoder_states.name] = hidden
-    
     if self.seq2seq_mode == 'vae':
       anneal_scale = 1
       if self.annealing and not forward_only:
@@ -518,7 +500,14 @@ class Seq2SeqModel(object):
       forward_only, hidden=hidden)
     
     # Output feed: depends on whether we do a backward step or not.
-    if not forward_only:                 
+    if not forward_only:    
+      if self.seq2seq_mode == 'vae' and self.global_step.eval() % 200 == 0:
+        kl_loss, reconstruct_loss = session.run([self.kl_loss[bucket_id],
+                                                 self.reconstruct_loss[bucket_id]],
+                                                input_feed)
+        logging.info("Step {}: KL loss {}, reconstruction loss {} ".format(self.global_step.eval(), 
+                                                                           kl_loss,
+                                                                           reconstruct_loss))
       output_feed = [self.updates[bucket_id],  # Update Op that does SGD.
                      self.gradient_norms[bucket_id],  # Gradient norm.
                      self.losses[bucket_id]]  # Loss for this batch.

tensorflow/models/rnn/translate/train.py
@@ -89,9 +89,8 @@ tf.app.flags.DEFINE_boolean("annealing", False, "Use KL cost annealing for VAE t
 tf.app.flags.DEFINE_boolean("scheduled_sample", True, "Use scheduled sampling, as in https://arxiv.org/abs/1506.03099")
 tf.app.flags.DEFINE_integer("scheduled_sample_steps", 1000, "Steps over which to linearly anneal the probability of decoding given the ground truth from 1.0 to 0.0")
 tf.app.flags.DEFINE_integer("kl_annealing_steps", 1000, "Steps over which to linearly anneal the KL loss from 0 to 1")
-tf.app.flags.DEFINE_float("word_keep_prob", 0.7, "Probability of not replacing decoder input word with UNK during training")
+tf.app.flags.DEFINE_float("word_keep_prob", 1.0, "Probability of not replacing decoder input word with UNK during training")
 tf.app.flags.DEFINE_float("kl_min", 0.0, "If >0, use minimum information criterion on KL loss as in https://arxiv.org/abs/1606.04934")
-tf.app.flags.DEFINE_float("word_keep_prob", 0.7, "Probability of not replacing decoder input word with UNK during training")
 
 # Optimization settings
 tf.app.flags.DEFINE_string("opt_algorithm", "sgd", "Optimization algorithm: sgd, adagrad, adadelta")

tensorflow/python/ops/seq2seq.py
@@ -113,9 +113,14 @@ def _extract_argmax_and_embed(embedding, output_projection=None,
     return emb_prev
   return loop_function
 
+def get_state_size(cell):
+  if isinstance(cell.state_size, tuple):
+    return cell.state_size[0]+cell.state_size[1]
+  else:
+    return cell.state_size
 
 def rnn_decoder(decoder_inputs, initial_state, cell, loop_function=None,
-                scope=None):
+                scope=None, feed_prev_p=None):
   """RNN decoder for the sequence-to-sequence model.
 
   Args:
@@ -141,19 +146,32 @@ def rnn_decoder(decoder_inputs, initial_state, cell, loop_function=None,
         (Note that in some cases, like basic RNN cell or GRU cell, outputs and
          states can be the same. They are different for LSTM cells though.)
   """
+
+
   with variable_scope.variable_scope(scope or "rnn_decoder"):
     state = initial_state
     outputs = []
     prev = None
     for i, inp in enumerate(decoder_inputs):
-      if loop_function is not None and prev is not None:
-        with variable_scope.variable_scope("loop_function", reuse=True):
-          inp = loop_function(prev, i)
-      if i > 0:
-        variable_scope.get_variable_scope().reuse_variables()
-      output, state = cell(inp, state)
-      outputs.append(output)
-      if loop_function is not None:
+      reuse = (loop_function is not None and prev is not None) or (i > 0)
+      with variable_scope.variable_scope(scope or "rnn_decoder", reuse=reuse):
+
+        def inner_loop(prev, inp, i, feed_prev=False):
+          if feed_prev and (prev is not None):
+            inp = loop_function(prev, i)
+          return inp
+
+        if feed_prev_p is not None:
+          rv = tf.random_uniform([], minval=0, maxval=1, dtype=tf.float32)
+          feed_prev = (loop_function is not None) and tf.less(rv, feed_prev_p)
+          inp = control_flow_ops.cond(feed_prev,
+                                      lambda: inner_loop(prev, inp, i, feed_prev=False),
+                                      lambda: inner_loop(prev, inp, i, feed_prev=True))
+        else:
+          feed_prev = (loop_function is not None)
+          inp = inner_loop(prev, inp, i, feed_prev)
+        output, state = cell(inp, state)
+        outputs.append(output)
         prev = output
   return outputs, state
 
@@ -227,7 +245,8 @@ def embedding_rnn_decoder(decoder_inputs,
                           output_projection=None,
                           feed_previous=False,
                           update_embedding_for_previous=True,
-                          scope=None):
+                          scope=None,
+                          feed_prev_p=None):
   """RNN decoder with embedding and a pure-decoding option.
 
   Args:
@@ -268,6 +287,7 @@ def embedding_rnn_decoder(decoder_inputs,
   Raises:
     ValueError: When output_projection has the wrong shape.
   """
+  schedule = True if feed_prev_p is not None else False
   with variable_scope.variable_scope(scope or "embedding_rnn_decoder") as scope:
     if output_projection is not None:
       dtype = scope.dtype
@@ -280,11 +300,12 @@ def embedding_rnn_decoder(decoder_inputs,
                                             [num_symbols, embedding_size])
     loop_function = _extract_argmax_and_embed(
         embedding, output_projection,
-        update_embedding_for_previous) if feed_previous else None
+        update_embedding_for_previous) if (feed_previous or schedule) else None
     emb_inp = (
         embedding_ops.embedding_lookup(embedding, i) for i in decoder_inputs)
     return rnn_decoder(emb_inp, initial_state, cell,
-                       loop_function=loop_function)
+                       loop_function=loop_function,
+                       feed_prev_p=feed_prev_p)
 
 
 def embedding_rnn_seq2seq(encoder_inputs,
@@ -397,7 +418,7 @@ def embedding_rnn_autoencoder_seq2seq(encoder_inputs, decoder_inputs, cell,
                                dtype=dtypes.float32, scope=None, encoder=None,
                                sequence_length=None, bucket_length=None,
                                init_backward=False, hidden_state=None, 
-                               initializer=None, legacy=False):
+                               initializer=None, legacy=False, feed_prev_p=None):
   with variable_scope.variable_scope(
       scope or "embedding_rnn_seq2seq", dtype=dtype) as scope:
     dtype = scope.dtype
@@ -413,7 +434,6 @@ def embedding_rnn_autoencoder_seq2seq(encoder_inputs, decoder_inputs, cell,
         encoder_inputs, dtype=dtype,
         sequence_length=sequence_length,
         bucket_length=bucket_length, legacy=legacy)
-      logging.info("Bidirectional state size=%d" % cell.state_size) # double the size for lstms
       if init_backward:
         cell = cell.get_bw_cell()
         initial_state = encoder_state_bw
@@ -428,7 +448,6 @@ def embedding_rnn_autoencoder_seq2seq(encoder_inputs, decoder_inputs, cell,
         encoder_cell, encoder_inputs, dtype=dtype,
         sequence_length=sequence_length, bucket_length=bucket_length,
         reverse=True)
-      logging.info("Unidirectional state size=%d" % cell.state_size)
       initial_state = encoder_state
 
     # Decoder.
@@ -446,7 +465,7 @@ def embedding_rnn_autoencoder_seq2seq(encoder_inputs, decoder_inputs, cell,
         outputs, state = embedding_rnn_decoder(
           decoder_inputs, initial_state, cell, num_symbols,
           embedding_size, output_projection=output_projection,
-          feed_previous=feed_previous)
+          feed_previous=feed_previous, feed_prev_p=feed_prev_p)
       return outputs, initial_state
 
     # If feed_previous is a Tensor, we construct 2 graphs and use cond.
@@ -466,7 +485,7 @@ def embedding_rnn_autoencoder_seq2seq(encoder_inputs, decoder_inputs, cell,
             decoder_inputs, initial_state, cell, num_symbols,
             embedding_size, output_projection=output_projection,
             feed_previous=feed_previous_bool,
-            update_embedding_for_previous=False)
+            update_embedding_for_previous=False, feed_prev_p=feed_prev_p)
         return outputs + [initial_state]
 
     outputs_and_state = control_flow_ops.cond(feed_previous,
@@ -482,7 +501,7 @@ def embedding_rnn_vae_seq2seq(encoder_inputs, decoder_inputs, cell,
                               dtype=dtypes.float32, scope=None, encoder=None,
                               sequence_length=None, bucket_length=None,
                               init_backward=False, latent_state=None, initializer=None,
-                              legacy=False):
+                              legacy=False, feed_prev_p=None, kl_min=0.0, anneal_scale=None):
   with variable_scope.variable_scope(
     scope or "embedding_rnn_seq2seq", dtype=dtype) as scope:
     dtype = scope.dtype
@@ -497,7 +516,7 @@ def embedding_rnn_vae_seq2seq(encoder_inputs, decoder_inputs, cell,
                                                     encoder_inputs, dtype=dtype,
                                                     sequence_length=sequence_length,
                                                     bucket_length=bucket_length,
-                                                                     legacy=legacy)
+                                                    legacy=legacy)
       logging.info("Bidirectional state size=%d" % cell.state_size) # double the size for lstms
       if init_backward:
         cell = cell.get_bw_cell()
@@ -514,15 +533,19 @@ def embedding_rnn_vae_seq2seq(encoder_inputs, decoder_inputs, cell,
         sequence_length=sequence_length, bucket_length=bucket_length,
         reverse=True)
       logging.info("Unidirectional state size=%d" % cell.state_size)
-         
+
     # Decoder.
     if output_projection is None:
       cell = rnn_cell.OutputProjectionWrapper(cell, num_symbols)
-
+ 
     # Latent state
-    z_mean_w = tf.get_variable('z_mean_w', [cell.state_size, latent_size])
+    if isinstance(cell.state_size, tuple):
+      encoder_out_state = tf.concat(concat_dim=1, values=encoder_out_state)
+
+    enc_state_size = get_state_size(cell)
+    z_mean_w = tf.get_variable('z_mean_w', [enc_state_size, latent_size])
     z_mean_b = tf.get_variable('z_mean_b', [latent_size])
-    z_logvar_w = tf.get_variable('z_logvar_w', [cell.state_size, latent_size])
+    z_logvar_w = tf.get_variable('z_logvar_w', [enc_state_size, latent_size])
     z_logvar_b = tf.get_variable('z_logvar_b', [latent_size])
 
     z_mean = transfer_func(tf.add(tf.matmul(encoder_out_state, z_mean_w), z_mean_b))
@@ -530,11 +553,22 @@ def embedding_rnn_vae_seq2seq(encoder_inputs, decoder_inputs, cell,
     eps = tf.random_normal(tf.shape(z_log_var), 0, 1, dtype=tf.float32)
     z = tf.add(z_mean, tf.mul(tf.sqrt(tf.exp(z_log_var)), eps))
 
-    kl_loss = -0.5 * tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), 1)
+    kl_loss = -0.5 * (1.0 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+    kl_step_av = tf.reduce_mean(kl_loss, [0])
+    if kl_min > 0.0:
+      kl_obj = tf.reduce_sum(tf.maximum(kl_step_av, kl_min))
+    else:
+      kl_obj = tf.reduce_sum(kl_step_av)
+    if anneal_scale is not None:
+      kl_obj = tf.multiply(anneal_scale, kl_obj)
 
-    dec_in_w = tf.get_variable('dec_in_w', [latent_size, cell.state_size])
-    dec_in_b = tf.get_variable('dec_in_b', [cell.state_size])
+    dec_state_size = get_state_size(cell)
+    dec_in_w = tf.get_variable('dec_in_w', [latent_size, dec_state_size])
+    dec_in_b = tf.get_variable('dec_in_b', [dec_state_size])
     initial_state = transfer_func(tf.add(tf.matmul(z, dec_in_w), dec_in_b))
+
+    if isinstance(cell.state_size, tuple):
+      initial_state = tuple(tf.split(1, 2, initial_state))
 
     if isinstance(feed_previous, bool):
       if latent_state is not None:
@@ -548,8 +582,8 @@ def embedding_rnn_vae_seq2seq(encoder_inputs, decoder_inputs, cell,
         outputs, state = embedding_rnn_decoder(
           decoder_inputs, initial_state, cell, num_symbols,
           embedding_size, output_projection=output_projection,
-          feed_previous=feed_previous)
-      return outputs, initial_state, kl_loss
+          feed_previous=feed_previous, feed_prev_p=feed_prev_p)
+      return outputs, initial_state, kl_obj
 
     # If feed_previous is a Tensor, we construct 2 graphs and use cond.
     def decoder(feed_previous_bool):
@@ -571,12 +605,12 @@ def embedding_rnn_vae_seq2seq(encoder_inputs, decoder_inputs, cell,
             decoder_inputs, initial_state, cell, num_symbols,
             embedding_size, output_projection=output_projection,
             feed_previous=feed_previous_bool,
-            update_embedding_for_previous=False)
+            update_embedding_for_previous=False, feed_prev_p=feed_prev_p)
         return outputs + [initial_state]
 
     outputs_and_state = control_flow_ops.cond(feed_previous,
-                                                       lambda: decoder(True),
-                                                       lambda: decoder(False))
+                                              lambda: decoder(True),
+                                              lambda: decoder(False))
     return outputs_and_state[:-1], outputs_and_state[-1], kl_loss
 
 
@@ -1441,7 +1475,8 @@ def sequence_loss(logits, targets, weights,
 
 def model_with_buckets_states(encoder_inputs, decoder_inputs, targets, weights,
                        buckets, seq2seq, softmax_loss_function=None,
-                              per_example_loss=False, name=None, encoder_states=None):
+                       per_example_loss=False, name=None, encoder_states=None,
+                       feed_prev_p=None):
   if len(encoder_inputs) < buckets[-1][0]:
     raise ValueError("Length of encoder_inputs (%d) must be at least that of la"
                      "st bucket (%d)." % (len(encoder_inputs), buckets[-1][0]))
@@ -1460,71 +1495,25 @@ def model_with_buckets_states(encoder_inputs, decoder_inputs, targets, weights,
     for j, bucket in enumerate(buckets):
       with variable_scope.variable_scope(variable_scope.get_variable_scope(),
                                          reuse=True if j > 0 else None):
-        if encoder_states is not None:
-          bucket_outputs, bucket_states = seq2seq(encoder_inputs[:bucket[0]],
-                                                  decoder_inputs[:bucket[1]],
-                                                  bucket[0],
-                                                  encoder_states)
-        else:
-          bucket_outputs, bucket_states = seq2seq(encoder_inputs[:bucket[0]],
-                                                  decoder_inputs[:bucket[1]],
-                                                  bucket[0])
-        outputs.append(bucket_outputs)
-        states.append(bucket_states)
+        seq2seq_out = seq2seq(encoder_inputs[:bucket[0]],
+                              decoder_inputs[:bucket[1]],
+                              bucket[0],
+                              encoder_states,
+                              feed_prev_p)
+        outputs.append(seq2seq_out[0])
+        states.append(seq2seq_out[1])
         if per_example_loss:
           losses.append(sequence_loss_by_example(
               outputs[-1], targets[:bucket[1]], weights[:bucket[1]],
-              softmax_loss_function=softmax_loss_function))
+              softmax_loss_function=softmax_loss_function))            
         else:
           losses.append(sequence_loss(
               outputs[-1], targets[:bucket[1]], weights[:bucket[1]],
               softmax_loss_function=softmax_loss_function))
-  return outputs, losses, states
-
-def vae_with_buckets_states(encoder_inputs, decoder_inputs, targets, weights,
-                       buckets, seq2seq, softmax_loss_function=None,
-                              per_example_loss=False, name=None, encoder_states=None):
-  if len(encoder_inputs) < buckets[-1][0]:
-    raise ValueError("Length of encoder_inputs (%d) must be at least that of la"
-                     "st bucket (%d)." % (len(encoder_inputs), buckets[-1][0]))
-  if len(targets) < buckets[-1][1]:
-    raise ValueError("Length of targets (%d) must be at least that of last"
-                     "bucket (%d)." % (len(targets), buckets[-1][1]))
-  if len(weights) < buckets[-1][1]:
-    raise ValueError("Length of weights (%d) must be at least that of last"
-                     "bucket (%d)." % (len(weights), buckets[-1][1]))
-
-  all_inputs = encoder_inputs + decoder_inputs + targets + weights
-  losses = []
-  outputs = []
-  states = []
-  with ops.op_scope(all_inputs, name, "model_with_buckets"):
-    for j, bucket in enumerate(buckets):
-      with variable_scope.variable_scope(variable_scope.get_variable_scope(),
-                                         reuse=True if j > 0 else None):
-        if encoder_states is not None:
-          bucket_outputs, bucket_states, bucket_kl_loss = seq2seq(encoder_inputs[:bucket[0]],
-                                                                  decoder_inputs[:bucket[1]],
-                                                                  bucket[0],
-                                                                  encoder_states)
-        else:
-          bucket_outputs, bucket_states, bucket_kl_loss = seq2seq(encoder_inputs[:bucket[0]],
-                                                                  decoder_inputs[:bucket[1]],
-                                                                  bucket[0])
-        outputs.append(bucket_outputs)
-        states.append(bucket_states)
-        if per_example_loss:
-          losses.append(sequence_loss_by_example(
-              outputs[-1], targets[:bucket[1]], weights[:bucket[1]],
-              softmax_loss_function=softmax_loss_function))
-        else:
-          bucket_kl_loss = tf.reduce_mean(bucket_kl_loss)
-          losses.append(sequence_loss(
-              outputs[-1], targets[:bucket[1]], weights[:bucket[1]],
-              softmax_loss_function=softmax_loss_function))
-
-
-        losses[-1] = (losses[-1], bucket_kl_loss)
+        if len(seq2seq_out) == 3: # include KL loss
+          kl_obj = seq2seq_out[2]
+          total_size = math_ops.add_n(weights[:bucket[1]]) + 1e-12  # avoid division by 0
+          losses[-1] = (losses[-1], tf.reduce_mean(kl_obj / total_size))
   return outputs, losses, states
 
 def model_with_buckets(encoder_inputs, decoder_inputs, targets, weights,